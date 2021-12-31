a6b9a01cb9b1e0c2be57a2d6cb64b6bf010fb8eb
zakariya ghanai <zakariyaghanai@gmail.com>
2021-12-31 23:08:25 +0100

    fix CustomInput icon rendering

--- client/src/components/Input/CustomInput.js
+++ client/src/components/Input/CustomInput.js
@@ -1,14 +1,15 @@
-import {React, Component} from "react";
+import React, { Component} from "react";
 import './CustomInput.css';
 
-import {EyeIcon, EyeOffIcon}  from '@heroicons/react/outline'
+import * as HeroIcon  from '@heroicons/react/outline'
+
 class CustomInput extends Component {
 
     iconDefaultClassName = ' absolute w-5 h-5 pointer-events-none left-2 '
     state = {
         name: this.props.name,
         icon: this.props.icon,
-        iconDefaultColor : this.props.icon.color
+        iconDefaultColor : this.props.icon && this.props.icon.color
             ? this.iconDefaultClassName + ' ' + this.props.icon.color
             : this.iconDefaultClassName + ' text-slate-400' ,
         placeholder: this.props.placeHolder ? this.props.placeHolder : '',
@@ -20,16 +21,17 @@ class CustomInput extends Component {
 
         return (
             <div className='custom-input flex items-center overflow-hidden'>
-                <this.state.icon.name
-                    className= {this.state.iconDefaultColor} />
+                {this.state.icon ? (
+                    React.createElement(this.state.icon.name, {className: this.state.iconDefaultColor})
+                ) : ''}
                 <input name={this.state.name} id={this.state.name} type={this.state.inputType} placeholder={this.state.placeholder}/>
                 {this.state.inputType === 'password' ? (
                     (this.state.showPassword
                         ? (
-                            <EyeIcon className='absolute w-5 h-5 right-2 text-slate-400 cursor-pointer hover:text-slate-800'/>
+                            <HeroIcon.EyeIcon className='absolute w-5 h-5 right-2 text-slate-400 cursor-pointer hover:text-slate-800'/>
                         )
                         : (
-                            <EyeOffIcon className='absolute w-5 h-5 right-2 text-slate-400 cursor-pointer hover:text-slate-800'/>
+                            <HeroIcon.EyeOffIcon className='absolute w-5 h-5 right-2 text-slate-400 cursor-pointer hover:text-slate-800'/>
                         ))
                 ) : ''}
             </div>
