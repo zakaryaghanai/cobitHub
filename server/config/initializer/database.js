12dbe8159cc88d5a5fc9e490831f6d19522de960
user.email <zakariyaghanai@gmail.com>
2022-07-04 11:14:34 +0100

    fix mognodb url

--- server/config/initializer/database.js
+++ server/config/initializer/database.js
@@ -1,19 +1,22 @@
-const mongoose = require('mongoose');
-const fs = require("fs");
+const mongoose = require("mongoose");
 
 const db_configuration = {
-  useUnifiedTopology: true,
-  useNewUrlParser: true
-}
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+};
 
 let url = process.env.COBITHUB_MONGO_URL;
 
-if(fs.existsSync('.env')) {
-  url = process.env.COBITHUB_MONGO_TEST_URL;
+if (process.env.NODE_ENV != "production") {
+    url = process.env.COBITHUB_MONGO_TEST_URL;
 }
 
 mongoose.connect(url, db_configuration);
-mongoose.connection.on('error', () => console.error.bind(console, 'Erreur lors de la connexion'));
-mongoose.connection.once('open', () => console.log("database connected successfully !!!"));
+mongoose.connection.on("error", () =>
+    console.error.bind(console, "error while connecting")
+);
+mongoose.connection.once("open", () =>
+    console.log("database connected successfully !!!")
+);
 
 module.exports = mongoose.connection;
